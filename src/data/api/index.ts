f4fd2f1b77bda7e3d17f74eebfd29934ca803580
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-15 17:55:45 -0300

    feat: launch links merge type policy

--- src/data/api/index.ts
+++ src/data/api/index.ts
@@ -7,21 +7,26 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {AsyncStorageWrapper, persistCache} from 'apollo3-cache-persist';
 import {useEffect, useState} from 'react';
 
-const URI = 'https://api.spacex.land/graphql';
+const uri = 'https://api.spacex.land/graphql';
+const cache = new InMemoryCache({
+  typePolicies: {
+    LaunchLinks: {
+      merge: true,
+    },
+  },
+});
 
 export const useApiClient = () => {
   const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
 
   useEffect(() => {
     const init = async () => {
-      const cache = new InMemoryCache();
-
       await persistCache({
         cache,
         storage: new AsyncStorageWrapper(AsyncStorage),
       });
 
-      setClient(new ApolloClient({uri: URI, cache}));
+      setClient(new ApolloClient({uri, cache}));
     };
 
     init();

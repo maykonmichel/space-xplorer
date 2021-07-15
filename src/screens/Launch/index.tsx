1f753b3e13e1e225f32ee17b4a6c24bf45623c9c
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-15 17:56:10 -0300

    feat: launch screen

--- src/screens/Launch/index.tsx
+++ src/screens/Launch/index.tsx
@@ -1,5 +1,9 @@
 import {Route, useRoute} from '@react-navigation/native';
 import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+import {useLaunchQuery} from '~/data/launch';
+import Slider from '~/molecules/Slider';
 
 import {LAUNCH} from '~/screens';
 
@@ -10,9 +14,30 @@ export type LaunchParams = {
 type RouteProp = Route<typeof LAUNCH, LaunchParams>;
 
 const Launch: FC = () => {
-  const {} = useRoute<RouteProp>();
+  const {
+    params: {id},
+  } = useRoute<RouteProp>();
 
-  return <></>;
+  const {data} = useLaunchQuery(id);
+
+  if (!data) return null;
+
+  const {
+    launch: {
+      rocket: {rocket_name},
+      links: {article_link, flickr_images},
+    },
+  } = data;
+
+  const images = flickr_images.slice(0, 3);
+
+  return (
+    <View>
+      <Text>{rocket_name}</Text>
+      <Slider data={images} />
+      <Text>{article_link}</Text>
+    </View>
+  );
 };
 
 export default Launch;

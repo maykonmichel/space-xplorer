644af9fb490e763842a4b207daaeb7cda8b16193
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-15 16:34:51 -0300

    feat: launches screen

--- src/screens/Launches/index.tsx
+++ src/screens/Launches/index.tsx
@@ -1,7 +1,28 @@
 import React, {FC} from 'react';
+import {FlatList, ListRenderItem} from 'react-native';
+
+import {Launch} from '~/data/launch';
+import {useLaunchesQuery} from '~/data/launch/launches';
+import LaunchCard from '~/molecules/LaunchCard';
+
+const keyExtractor = ({id}: Launch) => id;
+
+const renderItem: ListRenderItem<Launch> = ({item}) => <LaunchCard {...item} />;
 
 const Launches: FC = () => {
-  return <></>;
+  const {data} = useLaunchesQuery();
+
+  if (!data) return null;
+
+  const {launchesPast} = data;
+
+  return (
+    <FlatList
+      data={launchesPast}
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+    />
+  );
 };
 
 export default Launches;

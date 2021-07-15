55137261cc8787c7e4ab80d591315d6dbb325dd5
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-15 17:54:54 -0300

    feat: launch query

--- src/data/launch/index.ts
+++ src/data/launch/index.ts
@@ -1,5 +1,12 @@
+import {gql, useQuery} from '@apollo/client';
+
 type LaunchLinks = {
+  article_link: string;
   flickr_images: string[];
+};
+
+type LaunchRocket = {
+  rocket_name: string;
 };
 
 export type Launch = {
@@ -7,4 +14,31 @@ export type Launch = {
   mission_name: string;
   launch_date_local: string;
   links: LaunchLinks;
+  rocket: LaunchRocket;
 };
+
+const QUERY = gql`
+  query ($id: ID!) {
+    launch(id: $id) {
+      id
+      links {
+        article_link
+        flickr_images
+      }
+      rocket {
+        rocket_name
+      }
+    }
+  }
+`;
+
+type Data = {
+  launch: Launch;
+};
+
+type Variables = {
+  id: string;
+};
+
+export const useLaunchQuery = (id: string) =>
+  useQuery<Data, Variables>(QUERY, {variables: {id}});

95fc999da7b488bc76e6c16b6813ac21ed05d924
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-15 23:46:30 -0300

    feat: toggle favorite on press image

--- src/data/favoriteImages/index.ts
+++ src/data/favoriteImages/index.ts
@@ -1,4 +1,9 @@
-import {ApolloCache, gql, NormalizedCacheObject} from '@apollo/client';
+import {
+  ApolloCache,
+  gql,
+  NormalizedCacheObject,
+  useQuery,
+} from '@apollo/client';
 
 const QUERY = gql`
   {
@@ -17,3 +22,5 @@ const data: Data = {
 export const initializeFavoriteImages = (
   cache: ApolloCache<NormalizedCacheObject>,
 ) => cache.writeQuery<Data>({query: QUERY, data});
+
+export const useFavoriteImages = () => useQuery<Data>(QUERY);
--- src/molecules/Slider/index.tsx
+++ src/molecules/Slider/index.tsx
@@ -1,17 +1,36 @@
 import React, {FC} from 'react';
-import {FlatList, Image, ListRenderItem} from 'react-native';
+import {FlatList, Image, ListRenderItem, TouchableOpacity} from 'react-native';
+
+import {useFavoriteImages} from '~/data/favoriteImages';
 
 const keyExtractor = (uri: string) => uri;
-
-const renderItem: ListRenderItem<string> = ({item}) => (
-  <Image source={{uri: item}} style={{width: 200, height: 100}} />
-);
 
 type Props = {
   data: string[];
 };
 
 const Slider: FC<Props> = ({data}) => {
+  const {
+    client: {cache},
+    data: {favoriteImages = []} = {},
+  } = useFavoriteImages();
+
+  const toggleFavorite = (uri: string) =>
+    cache.modify({
+      fields: {
+        favoriteImages: () =>
+          favoriteImages.includes(uri)
+            ? favoriteImages.filter(image => image !== uri)
+            : [...favoriteImages, uri],
+      },
+    });
+
+  const renderItem: ListRenderItem<string> = ({item}) => (
+    <TouchableOpacity onPress={() => toggleFavorite(item)}>
+      <Image source={{uri: item}} style={{width: 200, height: 100}} />
+    </TouchableOpacity>
+  );
+
   return (
     <FlatList
       data={data}

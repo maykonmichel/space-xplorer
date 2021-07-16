db73b70d605d28d53515a3d20ef240bdbedf2df7
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-16 18:29:18 -0300

    feat: button atom

---
+++ src/atom/Button/index.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+import {ButtonProps, Text, TouchableOpacity, ViewStyle} from 'react-native';
+
+import styles from './styles';
+
+type Props = ButtonProps & {
+  style?: ViewStyle;
+};
+
+const Button: FC<Props> = ({title, style, ...props}) => {
+  return (
+    <TouchableOpacity
+      accessibilityRole={'button'}
+      style={[styles.container, style]}
+      {...props}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;

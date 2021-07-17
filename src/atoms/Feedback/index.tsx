e0920df6aeaf45acdddf2d42721d04e1923c2aee
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-17 17:06:25 -0300

    feat: error feedback go back button

--- src/atoms/Feedback/index.tsx
+++ src/atoms/Feedback/index.tsx
@@ -1,3 +1,4 @@
+import {useNavigation} from '@react-navigation/native';
 import LottieView from 'lottie-react-native';
 import React, {FC} from 'react';
 import {Text, View} from 'react-native';
@@ -5,6 +6,8 @@ import {Text, View} from 'react-native';
 import empty from '~/assets/animations/empty.json';
 import error from '~/assets/animations/error.json';
 import loading from '~/assets/animations/loading.json';
+
+import Button from '~/atoms/Button';
 
 import styles from './styles';
 
@@ -28,12 +31,21 @@ type Props = {
 };
 
 const Feedback: FC<Props> = ({type}) => {
+  const {canGoBack, goBack} = useNavigation();
+
+  const showGoBack = type === 'error' && canGoBack();
+
   const text = getText(type);
 
   return (
     <View style={styles.container}>
+      {showGoBack && <Text style={styles.text}>{text}</Text>}
       <LottieView source={sources[type]} style={styles.animation} autoPlay />
-      <Text style={styles.text}>{text}</Text>
+      {showGoBack ? (
+        <Button title={'Go back'} onPress={goBack} color={'light'} />
+      ) : (
+        <Text style={styles.text}>{text}</Text>
+      )}
     </View>
   );
 };

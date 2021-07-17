07ebd7e60c3bd88b703c64b4ecd4f50786f01c1d
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-17 17:58:11 -0300

    feat: launch image animate favorite button

--- src/molecules/LaunchImage/useAnimation.ts
+++ src/molecules/LaunchImage/useAnimation.ts
@@ -1,17 +1,57 @@
-import LottieView from 'lottie-react-native';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
+import {Animated} from 'react-native';
+
+import colors from '~/theme/colors';
+
+const duration = 500;
+const inputRange = [0, 1];
 
 export default (favorite: boolean) => {
-  const firstTime = useRef(true);
-  const animation = useRef<LottieView>(null);
+  const value = favorite ? 'Remove from favorites' : 'Add to favorites';
+
+  const [text, setText] = useState(value);
 
   useEffect(() => {
-    const [begin, end] = favorite ? [22, 50] : [0, 20];
-    const start = firstTime.current ? end : begin;
+    const timout = setTimeout(() => setText(value), duration / 2);
 
-    animation.current?.play(start, end);
-    firstTime.current = false;
+    return () => clearTimeout(timout);
+  }, [value]);
+
+  const animatedValue = useRef(new Animated.Value(+favorite));
+
+  useEffect(() => {
+    Animated.timing(animatedValue.current, {
+      duration,
+      toValue: +favorite,
+      useNativeDriver: false,
+    }).start();
   }, [favorite]);
 
-  return animation;
+  const backgroundColor = animatedValue.current.interpolate({
+    inputRange,
+    outputRange: ['transparent', colors.secondary.main],
+  });
+
+  const color = animatedValue.current.interpolate({
+    inputRange,
+    outputRange: [colors.secondary.main, colors.neutral.light],
+  });
+
+  const opacity = animatedValue.current.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 0, 1],
+  });
+
+  const progress = animatedValue.current.interpolate({
+    inputRange,
+    outputRange: favorite ? [0.32, 0.45] : [0.2, 0],
+  });
+
+  return {
+    backgroundColor,
+    color,
+    opacity,
+    progress,
+    text,
+  };
 };

e175af7a21f5f778f6033ce6aa6140b3b7c4817a
maykonmichel <maykonmichelpalma@gmail.com>
2021-07-17 07:37:52 -0300

    feat: add to / remove from favorites button

--- src/molecules/LaunchImage/index.tsx
+++ src/molecules/LaunchImage/index.tsx
@@ -1,27 +1,36 @@
 import React, {FC} from 'react';
-import {TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
 
 import styles from './styles';
 
 export type Props = {
+  favorite: boolean;
   onPress: (uri: string) => void;
   uri: string;
 };
 
-const LaunchImage: FC<Props> = ({onPress, uri}) => {
+const LaunchImage: FC<Props> = ({favorite, onPress, uri}) => {
   const emit = () => onPress(uri);
+
+  const text = favorite ? 'Remove from favorites' : 'Add to favorites';
 
   return (
     <TouchableOpacity
       onPress={emit}
       accessibilityLabel={'Toggle favorite'}
-      accessibilityRole={'imagebutton'}>
+      accessibilityRole={'imagebutton'}
+      style={styles.container}>
       <FastImage
         source={{uri}}
         style={styles.image}
         accessibilityRole={'image'}
       />
+      <View style={[styles.footer, favorite && styles.footerFavorite]}>
+        <Text style={[styles.text, favorite && styles.textFavorite]}>
+          {text}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
--- src/molecules/LaunchImage/styles.ts
+++ src/molecules/LaunchImage/styles.ts
@@ -1,8 +1,37 @@
 import {StyleSheet} from 'react-native';
 
+import colors from '~/theme/colors';
+
 export default StyleSheet.create({
+  container: {
+    borderRadius: 4,
+  },
+  footer: {
+    borderColor: colors.secondary.main,
+    borderWidth: 1,
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+  },
+  footerFavorite: {
+    backgroundColor: colors.secondary.main,
+  },
   image: {
     aspectRatio: 1920 / 1080,
     width: '100%',
+    borderColor: colors.secondary.main,
+    borderWidth: 1,
+    borderBottomWidth: 0,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+  },
+  text: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: colors.secondary.main,
+    marginVertical: 8,
+  },
+  textFavorite: {
+    color: colors.neutral.light,
   },
 });

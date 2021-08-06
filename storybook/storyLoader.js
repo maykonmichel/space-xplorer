03c855d0d2ab1f6c21811c29543b3c2cf87f7746
maykonmichel <maykonmichelpalma@gmail.com>
2021-08-06 09:28:52 -0300

    chore: setup story loader

---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/atoms/Button/stories');
+}
+
+const stories = ['../src/atoms/Button/stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
